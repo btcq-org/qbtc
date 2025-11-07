21cb54bc4618da9a68654364be32b6c3afabd4c0
jhernandezb <contact@jhernandez.me>
2025-11-07 13:11:17 -0600

    add genesis import/export

--- x/qbtc/types/genesis.go
+++ x/qbtc/types/genesis.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	fmt "fmt"
+	"net"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{}
@@ -8,5 +15,17 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	for _, nodeIP := range gs.NodeIPs {
+		if net.ParseIP(nodeIP.IP) == nil {
+			return fmt.Errorf("invalid IP address: %s", nodeIP.IP)
+		}
+		if nodeIP.Validator == "" {
+			return fmt.Errorf("validator cannot be empty")
+		}
+		_, err := sdk.ValAddressFromBech32(nodeIP.Validator)
+		if err != nil {
+			return fmt.Errorf("invalid validator address: %s", err)
+		}
+	}
 	return nil
 }
--- x/qbtc/types/genesis.pb.go
+++ x/qbtc/types/genesis.pb.go
@@ -78,7 +78,8 @@ func (m *Mimir) GetValue() int64 {
 
 // GenesisState defines the btcq module's genesis state.
 type GenesisState struct {
-	Mimirs []Mimir `protobuf:"bytes,1,rep,name=mimirs,proto3" json:"mimirs"`
+	Mimirs  []Mimir         `protobuf:"bytes,1,rep,name=mimirs,proto3" json:"mimirs"`
+	NodeIPs []GenesisNodeIP `protobuf:"bytes,2,rep,name=node_ips,json=nodeIps,proto3" json:"node_ips"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -121,30 +122,96 @@ func (m *GenesisState) GetMimirs() []Mimir {
 	return nil
 }
 
+func (m *GenesisState) GetNodeIPs() []GenesisNodeIP {
+	if m != nil {
+		return m.NodeIPs
+	}
+	return nil
+}
+
+type GenesisNodeIP struct {
+	Validator string `protobuf:"bytes,1,opt,name=validator,proto3" json:"validator,omitempty"`
+	IP        string `protobuf:"bytes,2,opt,name=ip,proto3" json:"ip,omitempty"`
+}
+
+func (m *GenesisNodeIP) Reset()         { *m = GenesisNodeIP{} }
+func (m *GenesisNodeIP) String() string { return proto.CompactTextString(m) }
+func (*GenesisNodeIP) ProtoMessage()    {}
+func (*GenesisNodeIP) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8307623358d2b26a, []int{2}
+}
+func (m *GenesisNodeIP) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *GenesisNodeIP) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_GenesisNodeIP.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *GenesisNodeIP) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GenesisNodeIP.Merge(m, src)
+}
+func (m *GenesisNodeIP) XXX_Size() int {
+	return m.Size()
+}
+func (m *GenesisNodeIP) XXX_DiscardUnknown() {
+	xxx_messageInfo_GenesisNodeIP.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GenesisNodeIP proto.InternalMessageInfo
+
+func (m *GenesisNodeIP) GetValidator() string {
+	if m != nil {
+		return m.Validator
+	}
+	return ""
+}
+
+func (m *GenesisNodeIP) GetIP() string {
+	if m != nil {
+		return m.IP
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*Mimir)(nil), "qbtc.qbtc.v1.mimir")
 	proto.RegisterType((*GenesisState)(nil), "qbtc.qbtc.v1.GenesisState")
+	proto.RegisterType((*GenesisNodeIP)(nil), "qbtc.qbtc.v1.GenesisNodeIP")
 }
 
 func init() { proto.RegisterFile("qbtc/qbtc/v1/genesis.proto", fileDescriptor_8307623358d2b26a) }
 
 var fileDescriptor_8307623358d2b26a = []byte{
-	// 230 bytes of a gzipped FileDescriptorProto
+	// 326 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2a, 0x4c, 0x2a, 0x49,
 	0xd6, 0x07, 0x13, 0x65, 0x86, 0xfa, 0xe9, 0xa9, 0x79, 0xa9, 0xc5, 0x99, 0xc5, 0x7a, 0x05, 0x45,
 	0xf9, 0x25, 0xf9, 0x42, 0x3c, 0x20, 0x61, 0x3d, 0x30, 0x51, 0x66, 0x28, 0x25, 0x98, 0x98, 0x9b,
 	0x99, 0x97, 0xaf, 0x0f, 0x26, 0x21, 0x0a, 0xa4, 0x44, 0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0x4c, 0x7d,
 	0x10, 0x0b, 0x22, 0xaa, 0xa4, 0xcf, 0xc5, 0x9a, 0x9b, 0x99, 0x9b, 0x59, 0x24, 0x24, 0xc0, 0xc5,
 	0x9c, 0x9d, 0x5a, 0x29, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0x04, 0x62, 0x0a, 0x89, 0x70, 0xb1,
-	0x96, 0x25, 0xe6, 0x94, 0xa6, 0x4a, 0x30, 0x29, 0x30, 0x6a, 0x30, 0x07, 0x41, 0x38, 0x4a, 0x8e,
-	0x5c, 0x3c, 0xee, 0x10, 0x8b, 0x83, 0x4b, 0x12, 0x4b, 0x52, 0x85, 0x0c, 0xb9, 0xd8, 0xc0, 0x06,
-	0x14, 0x4b, 0x30, 0x2a, 0x30, 0x6b, 0x70, 0x1b, 0x09, 0xeb, 0x21, 0x3b, 0x44, 0x0f, 0x2c, 0xe7,
-	0xc4, 0x72, 0xe2, 0x9e, 0x3c, 0x43, 0x10, 0x54, 0xa1, 0x93, 0xfd, 0x89, 0x47, 0x72, 0x8c, 0x17,
-	0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c,
-	0x37, 0x1e, 0xcb, 0x31, 0x44, 0xa9, 0xa6, 0x67, 0x96, 0x64, 0x94, 0x26, 0xe9, 0x25, 0xe7, 0xe7,
-	0xea, 0x27, 0x95, 0x24, 0x17, 0xea, 0xe6, 0x17, 0xa5, 0x43, 0xfc, 0x5b, 0x01, 0xa1, 0x4a, 0x2a,
-	0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0x6e, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x76, 0xdd,
-	0x85, 0x35, 0x10, 0x01, 0x00, 0x00,
+	0x96, 0x25, 0xe6, 0x94, 0xa6, 0x4a, 0x30, 0x29, 0x30, 0x6a, 0x30, 0x07, 0x41, 0x38, 0x4a, 0xd3,
+	0x18, 0xb9, 0x78, 0xdc, 0x21, 0x36, 0x07, 0x97, 0x24, 0x96, 0xa4, 0x0a, 0x19, 0x72, 0xb1, 0x81,
+	0x4d, 0x28, 0x96, 0x60, 0x54, 0x60, 0xd6, 0xe0, 0x36, 0x12, 0xd6, 0x43, 0x76, 0x89, 0x1e, 0x58,
+	0xce, 0x89, 0xe5, 0xc4, 0x3d, 0x79, 0x86, 0x20, 0xa8, 0x42, 0xa1, 0x60, 0x2e, 0x8e, 0xbc, 0xfc,
+	0x94, 0xd4, 0xf8, 0xcc, 0x82, 0x62, 0x09, 0x26, 0xb0, 0x26, 0x69, 0x54, 0x4d, 0x50, 0x0b, 0xfc,
+	0xf2, 0x53, 0x52, 0x3d, 0x03, 0x9c, 0xa4, 0x41, 0x9a, 0x1f, 0xdd, 0x93, 0x67, 0x87, 0xf0, 0x8b,
+	0x5f, 0xdd, 0x93, 0x87, 0xeb, 0x0f, 0x62, 0x07, 0xb1, 0x3c, 0x0b, 0x8a, 0x95, 0x5c, 0xb9, 0x78,
+	0x51, 0xb4, 0x09, 0xc9, 0x70, 0x71, 0x96, 0x25, 0xe6, 0x64, 0xa6, 0x24, 0x96, 0xe4, 0x17, 0x41,
+	0xfd, 0x85, 0x10, 0x10, 0x12, 0xe3, 0x62, 0xca, 0x2c, 0x00, 0x7b, 0x8d, 0xd3, 0x89, 0xed, 0xd1,
+	0x3d, 0x79, 0x26, 0xcf, 0x80, 0x20, 0xa6, 0xcc, 0x02, 0x27, 0xfb, 0x13, 0x8f, 0xe4, 0x18, 0x2f,
+	0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18,
+	0x6e, 0x3c, 0x96, 0x63, 0x88, 0x52, 0x4d, 0xcf, 0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf,
+	0xd5, 0x4f, 0x2a, 0x49, 0x2e, 0xd4, 0xcd, 0x2f, 0x4a, 0x87, 0x44, 0x46, 0x05, 0x84, 0x2a, 0xa9,
+	0x2c, 0x48, 0x2d, 0x4e, 0x62, 0x03, 0x07, 0xac, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xb8, 0x98,
+	0x40, 0x83, 0xad, 0x01, 0x00, 0x00,
 }
 
 func (m *Mimir) Marshal() (dAtA []byte, err error) {
@@ -202,6 +269,20 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.NodeIPs) > 0 {
+		for iNdEx := len(m.NodeIPs) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.NodeIPs[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x12
+		}
+	}
 	if len(m.Mimirs) > 0 {
 		for iNdEx := len(m.Mimirs) - 1; iNdEx >= 0; iNdEx-- {
 			{
@@ -215,6 +296,43 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 			i--
 			dAtA[i] = 0xa
 		}
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *GenesisNodeIP) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *GenesisNodeIP) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *GenesisNodeIP) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.IP) > 0 {
+		i -= len(m.IP)
+		copy(dAtA[i:], m.IP)
+		i = encodeVarintGenesis(dAtA, i, uint64(len(m.IP)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Validator) > 0 {
+		i -= len(m.Validator)
+		copy(dAtA[i:], m.Validator)
+		i = encodeVarintGenesis(dAtA, i, uint64(len(m.Validator)))
+		i--
+		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
 }
@@ -257,6 +375,29 @@ func (m *GenesisState) Size() (n int) {
 			l = e.Size()
 			n += 1 + l + sovGenesis(uint64(l))
 		}
+	}
+	if len(m.NodeIPs) > 0 {
+		for _, e := range m.NodeIPs {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *GenesisNodeIP) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Validator)
+	if l > 0 {
+		n += 1 + l + sovGenesis(uint64(l))
+	}
+	l = len(m.IP)
+	if l > 0 {
+		n += 1 + l + sovGenesis(uint64(l))
 	}
 	return n
 }
@@ -430,6 +571,154 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 			if err := m.Mimirs[len(m.Mimirs)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NodeIPs", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.NodeIPs = append(m.NodeIPs, GenesisNodeIP{})
+			if err := m.NodeIPs[len(m.NodeIPs)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipGenesis(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *GenesisNodeIP) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowGenesis
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: GenesisNodeIP: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: GenesisNodeIP: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Validator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Validator = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IP", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.IP = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
