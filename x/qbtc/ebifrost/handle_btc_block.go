ddb8f9fd71ef523a8d285383ed2c2309fd3eca8c
jhernandezb <contact@jhernandez.me>
2025-11-10 16:00:42 -0600

    add ante handler and msgs to proposed txs

---
+++ x/qbtc/ebifrost/handle_btc_block.go
@@ -0,0 +1,10 @@
+package ebifrost
+
+import (
+	"github.com/btcq-org/qbtc/x/qbtc/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (eb *EnshrinedBifrost) HandleBTCBlock(ctx sdk.Context, block *types.MsgBtcBlock) error {
+	return nil
+}
