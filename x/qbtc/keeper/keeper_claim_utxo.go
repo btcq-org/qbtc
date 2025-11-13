6a63a27bcc16fc6bde8e33e3e8c7a1267caae338
jhernandezb <contact@jhernandez.me>
2025-11-13 14:18:15 -0600

    do not error when entitled amount is 0

--- x/qbtc/keeper/keeper_claim_utxo.go
+++ x/qbtc/keeper/keeper_claim_utxo.go
@@ -2,13 +2,13 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
-	"cosmossdk.io/math"
 	"github.com/btcq-org/qbtc/x/qbtc/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// ClaimUTXO claims a UTXO by the governance module.
+// It mints the coins to the reserve module account and resets the entitled amount to 0.
 func (k Keeper) ClaimUTXO(ctx context.Context, txid string, vout uint32) error {
 	key := getUTXOKey(txid, vout)
 	utxo, err := k.Utxoes.Get(ctx, key)
@@ -16,11 +16,13 @@ func (k Keeper) ClaimUTXO(ctx context.Context, txid string, vout uint32) error {
 		return err
 	}
 	if utxo.EntitledAmount == 0 {
-		return fmt.Errorf("UTXO not entitled to claim")
+		return nil
 	}
 
+	// denom is set in app/config.go
+	coin := sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(utxo.EntitledAmount))
 	// mint the coins to the module account
-	if err := k.bankKeeper.MintCoins(ctx, types.ReserveModuleName, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(int64(utxo.EntitledAmount))))); err != nil {
+	if err := k.bankKeeper.MintCoins(ctx, types.ReserveModuleName, sdk.NewCoins(coin)); err != nil {
 		return err
 	}
 
